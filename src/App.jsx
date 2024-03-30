d4599a3f9160c827b345f20ff90b5f26ef329d56
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-03-30 13:16:19 -0400

    add sort

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import items from './data.json'
 import Item from './components/Item'
 import SearchBar from './components/SearchBar'
+import SortButton from './components/SortButton'
 
 function App() {
   const listProducts = items["products"]
@@ -10,9 +11,10 @@ function App() {
     <>
       <h1>List of product</h1>
       <SearchBar/>
+      <SortButton/>
       <ul>
-        {listProducts.map((product)=>{
-          return <Item name={product.name} score={product.impact_score}/>
+        {listProducts.map((product, index)=>{
+          return <Item key={index} name={product.name} score={product.impact_score}/>
         })}
       </ul>
        
---
+++ src/components/SortButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SortButton = () => {
+  return (
+    <>
+        <button>Sort</button>
+    </>
+  )
+}
+
+export default SortButton
