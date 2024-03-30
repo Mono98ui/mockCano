5ecdaa51a5145feadc670512aadbd1846a407054
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-03-30 16:58:10 -0400

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,52 +5,70 @@ import SortButton from './components/SortButton'
 import Items from './components/Items'
 
 function App() {
+
   const [listProducts, setListProducts] = useState(items["products"])
   const [listProductsInit, setListProductsInit] = useState(items["products"])
   const [orderBy, setOrderBy] = useState(0)
 
   const changeOrder = ()=>{
+
     let nbr = orderBy+1
     if(nbr > 2){
       nbr = 0
     }
     setOrderBy(nbr)
     sortList(nbr)
+
   }
 
   const applySearch = (searchWord) =>{
     let tmpList = []
+
+    //Search product containing the specify word
     if(searchWord.length!==0){
       for(let i =0; i < items["products"].length; i++){
         if(items["products"][i].name.includes(searchWord)){
           tmpList.push(items["products"][i])
         }
       }
+
     setListProducts(tmpList)
     setListProductsInit(tmpList)
+
     }else{
+
       setListProducts(items["products"])
       setListProductsInit(items["products"])
+
     }
   }
 
   const sortList = (orderBy) =>{
     let sortArray = []
+
+    //Ascending
     if(orderBy === 1){
+
       sortArray =listProducts.map((product)=>{
         return product
       }).sort(function (product1, product2) {
         return product1.impact_score > product2.impact_score;
       })
+
+    //Descending
     }else if( orderBy === 2){
+
       sortArray =listProducts.map((product)=>{
         return product
       }).sort(function (product1, product2) {
         return product1.impact_score < product2.impact_score;
       })
+
     }else{
+
       sortArray = listProductsInit
     }
+
     setListProducts(sortArray)
   }
 
@@ -58,7 +76,7 @@ function App() {
 
   return (
     <>
-      <h1>List of product</h1>
+      <h1>List of products</h1>
       <SearchBar applySearch={applySearch}/>
       <SortButton changeOrder={changeOrder}/>
       <Items listProducts={listProducts}/>
