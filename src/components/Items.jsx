bf9bdb069114a35d6626d0ebe845f0fa8c749de0
Kha Pham <49766423+Mono98ui@users.noreply.github.com>
2024-03-31 21:31:41 -0400

    format code

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -4,7 +4,7 @@ import Item from './Item'
 const Items = ({ listProducts }) => {
   return (
     <>
-       <div className="container">
+      <div className="container">
         {listProducts.map((product, index) => {
           return <Item key={index} name={product.name} impact_score={product.impact_score} />
         })}
